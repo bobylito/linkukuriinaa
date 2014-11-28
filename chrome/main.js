8f7d111896005bf1f4e950f9b83c84a2d28de8cb
Étienne Vallette d'Osia <dohzya@gmail.com>
2014-11-28 21:46:00 +0900

    Add some code

--- chrome/main.js
+++ chrome/main.js
@@ -1,7 +1,23 @@
-//Some stupid stuff
-console.log("test");
+/* jshint asi:true */
 
-var links = document.querySelectorAll('#rcnt a');
-for (var i=0; i < links.length; i++) {
-  links[i].onmousedown = undefined;
-}
+console.log("リンククリーナー ready!")
+
+window.LinkuKuriinaa = (function (){
+  var LinkuKuriinaa = {}
+
+  LinkuKuriinaa.cleanLink = function (link) {
+    link.removeAttribute("onmousedown")
+    link.onmousedown = undefined
+  }
+
+  LinkuKuriinaa.cleanAllLinks = function (base) {
+    var links = document.querySelectorAll(base)
+    for (var i=0; i < links.length; i++) {
+      LinkuKuriinaa.cleanLink(links[i])
+    }
+  }
+
+  return LinkuKuriinaa
+}())
+
+window.LinkuKuriinaa.cleanAllLinks('#res a')
