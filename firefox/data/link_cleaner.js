edd27db3951970e97bda8d72bf1983ef09deee49
Joris Guyonvarch <jgu@zengularity.com>
2014-11-28 14:23:01 +0100

    Disable onmousdown at input change after some time

--- firefox/data/link_cleaner.js
+++ firefox/data/link_cleaner.js
@@ -1,11 +1,13 @@
-document.querySelector('body').onmousedown = function(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  console.log('on click');
-}
+disableOnMouseDown();
 
-// var links = document.querySelectorAll('#rcnt a');
-// for (var i=0; i < links.length; i++) {
-//   console.log("link: " + links[i]);
-//   links[i].onmousedown = undefined;
-// }
+document.querySelector('#gbqfq').addEventListener('input', function() {
+  setTimeout(disableOnMouseDown, 1000);
+});
+
+function disableOnMouseDown() {
+  console.log('removeOnMouseDown');
+  var links = document.querySelectorAll('#rcnt a');
+  for (var i=0; i < links.length; i++) {
+    links[i].onmousedown = undefined;
+  }
+}
