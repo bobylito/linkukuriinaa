7102b537b07106466ef1a187cac8b9bb5eb4e7d4
Mayleen Richard Lacouture <mri@zenexity.com>
2014-11-28 15:31:45 +0100

    Remove onmouse event on every a element.

--- firefox/data/link_cleaner.js
+++ firefox/data/link_cleaner.js
@@ -3,8 +3,7 @@ window.setInterval(function() {
 }, 100);
 
 function disableOnMouseDown() {
-  console.log('disableOnMouseDown');
-  var links = document.querySelectorAll('#rcnt a');
+  var links = document.querySelectorAll('a');
   for (var i=0; i < links.length; i++) {
     links[i].onmousedown = undefined;
   }
