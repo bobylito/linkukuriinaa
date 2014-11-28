3228642818750d7a8a9c47972d1256c805bc6a37
Joris Guyonvarch <jgu@zengularity.com>
2014-11-28 13:27:04 +0100

    Catch onmousedown on body and stop propagation

--- firefox/data/link_cleaner.js
+++ firefox/data/link_cleaner.js
@@ -1,7 +1,11 @@
-console.log("I'm in...");
-
-var links = document.querySelectorAll('#rcnt a');
-for (var i=0; i < links.length; i++) {
-	console.log("link: " + links[i]);
-	//links[i].onmousedown = undefined;
+document.querySelector('body').onmousedown = function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log('on click');
 }
+
+// var links = document.querySelectorAll('#rcnt a');
+// for (var i=0; i < links.length; i++) {
+//   console.log("link: " + links[i]);
+//   links[i].onmousedown = undefined;
+// }
