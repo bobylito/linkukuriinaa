7bbc374baf81834417994a2a5613807496449638
Joris Guyonvarch <jgu@zengularity.com>
2014-11-28 17:34:39 +0100

    Add a state and handle toggle (only work for the button, the job don't stop nor start on toggle

--- firefox/data/link_cleaner.js
+++ firefox/data/link_cleaner.js
@@ -3,6 +3,7 @@ window.setInterval(function() {
 }, 100);
 
 function disableOnMouseDown() {
+  console.log('clean !');
   var links = document.querySelectorAll('a');
   for (var i=0; i < links.length; i++) {
     links[i].onmousedown = undefined;
